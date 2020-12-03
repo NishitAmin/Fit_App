781244585403309e2dd82d013aef03fbc5ef51fd
NishitAmin <nishit964@gmail.com>
2020-12-03 09:46:27 -0500

    UI updated, and splash activity.

--- app/src/main/java/org/rohan/patel/finalprojectandroid/MainActivity.kt
+++ app/src/main/java/org/rohan/patel/finalprojectandroid/MainActivity.kt
@@ -3,12 +3,27 @@ package org.rohan.patel.finalprojectandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import org.rohan.patel.finalprojectandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        val navController = this.findNavController(R.id.NavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.NavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
